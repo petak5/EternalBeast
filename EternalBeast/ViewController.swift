d311390f0ef9182f33cd79e737963bab738ad116
petak5 <peterurgos12345@gmail.com>
2021-05-02 09:02:40 +0200

    Initial commit

---
+++ EternalBeast/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EternalBeast
+//
+//  Created by Peter Urgoš on 02/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
